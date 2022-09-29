9f607584412b1837d7e740a03d8ca19a918dafcb
rovin76 <rovin7678>
2022-09-29 16:00:23 +0530

    day-2

--- Client/src/Compoents/Landing/Landing.jsx
+++ Client/src/Compoents/Landing/Landing.jsx
@@ -1,24 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import  Navbar  from '../Navbar/Navbar'
-
-import Frontpage from "../Pages/Frontpage/Frontpage"
-
-import Test from '../Pages/Section/Section';
-import SplitScreen from '../Pages/Mproject/Mproject';
-import Footer from '../Pages/Footer/Footer';
-
+import Navbar from "../Navbar/Navbar";
+import Frontpage from "../Pages/Frontpage/Frontpage";
+import Test from "../Pages/Section/Section";
+import SplitScreen from "../Pages/Mproject/Mproject";
+import Footer from "../Pages/Footer/Footer";
 const Landing = () => {
-  return (
-    <div>
-             <Navbar/>
-<Frontpage/>
-<Test/>
-{/* <Section/> */}
-<SplitScreen/>
-<Footer/>
-    </div>
-  )
-}
+	return (
+		<div>
+			<Navbar />
+			<Frontpage />
+			<Test />
+			{/* <Section/> */}
+			<SplitScreen />
+			<Footer />
+		</div>
+	);
+};
 
-export default Landing
+export default Landing;
