5f24d980baac007f6731b37892125e579442cfca
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-09-30 17:38:57 +0530

    route

--- Client/src/Compoents/Routing.jsx
+++ Client/src/Compoents/Routing.jsx
@@ -8,17 +8,20 @@ import Reports from "./CRM/Reports";
 import Settings from "./CRM/Settings";
 import Team from "./CRM/Team";
 import Time from "./CRM/Time";
+import Demo from "./Pages/Demo/DemoPage";
 const Routing = () => {
 	return (
 		<>
 			<Routes>
 				<Route path='/' element={<Landing />} />
+				<Route path='/demo' element={<Demo/>} />
 				{/* this is crm Routing pls make sure you does not add your rout between them */}
 				<Route path='/home' element={<Home />} />
 				<Route path='/time' element={<Time />} />
 				<Route path='/projects' element={<Projects />} />
 				<Route path='/reports' element={<Reports />} />
 				<Route path='/team' element={<Team />} />
+			
 				<Route path='/settings' element={<Settings />} />
 				<Route path='/clients' element={<Clients />} />
 			</Routes>
