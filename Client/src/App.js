f8e97fda27910a65f05a263dc6b5ed90c8f4ec50
vedantpawar18 <vedantpawar18@gmail.com>
2022-09-30 16:06:30 +0530

    finalizing pricing page

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,8 +1,10 @@
+import PricesPage from "./Compoents/Pages/PricesPage";
 import Routing from "./Compoents/Routing";
 function App() {
 	return (
 		<div className='App'>
-			<Routing />
+			{/* <Routing /> */}
+			<PricesPage />
 		</div>
 	);
 }
